c3172d120caa3dc8f19c07b11c22369c531ce028
pengge4533 <122876863+pengge4533@users.noreply.github.com>
2023-05-12 15:52:22 +0800

    Update peng.js

--- peng.js
+++ peng.js
@@ -1,1 +1,8 @@
-
+/*********************************************
+@Zoo
+Soul解除送礼私聊限制
+日期:2022.04.03
+[rewrite_local]
+^https:\/\/api-chat\.soulapp\.cn\/chat\/limitInfo url script-response-body https://raw.githubusercontent.com/pengge4533/Zijide/main/pengge.js?token=GHSAT0AAAAAACCQTW7W4UXQQCFHYXQHZQIAZC54Y2Q
+hostname = api-chat.soulapp.cn
+*********************************************/
